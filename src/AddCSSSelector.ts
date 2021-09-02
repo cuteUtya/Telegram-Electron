6eb900ba5441c86b61c474086697e13fe62ad75d
Tim-dev-hub <timurteresenko@gmail.com>
2021-09-02 18:29:29 +0300

    ⚙ Write my own JSS realization

---
+++ src/AddCSSSelector.ts
@@ -0,0 +1,45 @@
+let styleElement = document.createElement('style');
+styleElement.type = 'text/css';
+document.getElementsByTagName('head')[0].appendChild(styleElement);
+
+
+export function AddCSSSelector (selector :string, css: any) {
+    styleElement.innerHTML += selector.toString() + getCSS(css);
+}
+
+
+function getCSS(selector) {
+    var css = [];
+        let style = " {";
+
+        for (let prop in selector) {
+            let classname = prop;
+
+            let words = [];
+            let word = prop[0];
+            for(let i = 1; i < prop.length; i++){
+                if(prop[i] === prop[i].toUpperCase()) {
+                    words.push(word);
+                    word = "";
+                }
+
+                    word += prop[i];
+            }
+            words.push(word);
+
+            if(words.length > 1){
+                classname = words[0];
+
+                for (let i = 1; i < words.length; i++){
+                    classname += "-" + words[i];
+                }
+            }
+            //add px to every number
+            let measure = isNaN(selector[prop]) ? '' : 'px';
+            style += classname + ":" + selector[prop] + measure + "; ";
+        }
+        style += "}";
+        css.push(style);
+
+    return css.join("\n");
+}
