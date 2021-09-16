8a719bde1e58ab9c359ac4fb1c2ccf5d2a26914e
Tim-dev-hub <timurteresenko@gmail.com>
2021-09-16 19:07:59 +0300

    🔧 added the possibility of custom parameters to InitClient function

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,12 +1,9 @@
-import Client from "tdl";
+import Client, {ConfigType} from "tdl";
 import {TDLib} from "tdl-tdlib-addon";
 
-export default function InitClient() : Promise<Client>{
-    let client = new Client(new TDLib("tdjson.dll", "C:\\Users\\Timur\\electronProjects\\Electrogram\\node_modules\\tdl-tdlib-addon\\build\\Release\\td.node"), {
-        apiId: 6627546,
-        apiHash: "7c84903108b41b3872080707a6da6ad6",
+export default function InitClient(options?: ConfigType) : Promise<Client>{
 
-    })
+    let client = new Client(new TDLib("tdjson.dll", "C:\\Users\\Timur\\electronProjects\\Electrogram\\node_modules\\tdl-tdlib-addon\\build\\Release\\td.node"), options)
 
     return new Promise<Client>((resolve, reject) => {
         client.connect().then(() => resolve(client));
