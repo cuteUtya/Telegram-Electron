fd3ab78e053ccc66052d0989b2b6978cac2689b9
Tim-dev-hub <timurteresenko@gmail.com>
2021-09-21 16:30:11 +0300

    🐞 Add app version to constats

--- src/AppConstanst.tsx
+++ src/AppConstanst.tsx
@@ -1,3 +1,4 @@
 export const apiId: number = 6627546;
 export const apiHash: string = "7c84903108b41b3872080707a6da6ad6";
-export const UseTestDC : boolean = true;
+export const UseTestDC : boolean = false;
+export const Version : string = "Development alpha 0.0.1";
